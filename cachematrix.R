6ec9d951b904f32b492dbd77fa615f7b2e2a9804
Dan <danzalewski217@gmail.com>
2015-01-25 23:56:11 +0700

    correct directory

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My first function makes a matrix with the input data and caches it. 
+##My second data calls the cache and inverts the matrix.
 
-## Write a short comment describing this function
-
+## This function creates a matrix in both containing and existing environments. 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function calls the cache matrix from the last function and inverts it using the solve function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix()
+    if(!is.null(m)) {
+      message("getting cached data")
+      solve(m)
+    }
+    data <- x$get()
+    m <- matrix(data, ...)
+    x$setmatrix(m)
+    solve(m)
+  }
 }
+
